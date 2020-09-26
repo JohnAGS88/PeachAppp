6c6ff72860fe80f83bda71572c9a6a316c88e9c1
JohnGoyo <jgoyosilva@gmail.com>
2020-09-26 07:07:32 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <div id="nav">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="image is-128x128">
+      <img src="/assets/peach.jpg">
+    </a>
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+      <router-link to="/">Home</router-link> 
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+          <router-link to="/sign">Sign up</router-link>
+            <strong></strong>
+          </a>
+          <a class="button is-light">
+          <router-link to="/login">Log in</router-link>
+            
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+<section class="hero is-primary is-bold is-medium">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title has-text-centered">
+        Haz la Diferencia
+      </h1>
+      <h2 class="subtitle has-text-centered">
+        Conviertete 
+      </h2>
+    </div>
+  </div>
+  <section class="hero is-medium is-light is-bold">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title has-text-black has-text-centered">
+        Busca
+      </h1>
+      <h2 class="subtitle has-text-black has-text-centered">
+        Conoce algo Mas!
+      </h2>
+    </div>
+  </div>
+  <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>PEACHAPP</strong> by <strong>JAGS</strong>. looking for shape
+      <a href="https://github.com/JohnAGS88">GIThub</a>. The website content
+      is licensed
+    </p>
+  </div>
+</footer>
+</section>
+</section>
+
